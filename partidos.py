86e45d301fc505e87103b16b90aeb1af12aeb35d
fraan28 <ftebmur2807@g.educaand.es>
2025-12-15 13:55:16 +0100

    Función "leerPartidos" mejorada y actualizada mostrando el help

--- partidos.py
+++ partidos.py
@@ -1,15 +1,30 @@
 import csv
 
-# LeerPartidos(): Función que lee el fichero CSV y devuelve los datos del mismo en una lista de diccionarios. (Adrián)
-def LeerPartidos(fichero):
+# LeerPartidos(): Función que lee el fichero CSV y devuelve los datos del mismo en una lista de diccionarios. (Fran)
+def leerPartidos(fichero):
+    """ 
+    Leer los partidos del fichero pasado por parámetro
+
+    Parameters:
+        fichero (str): Fichero de la liga en CSV
+
+    Returns:
+        list: Lista de los partidos
+    """
     partidos = []
     try:
         with open(fichero, mode="r") as f:
             lector = csv.DictReader(f)
             for fila in lector:
-                partidos.append(fila)
+                goles = fila['FT'].split('-')
+                golesParte2 = fila['HT'].split('-')
+                partidos.append({
+                    'local': fila['Team 1'],
+                    'visitante': fila['Team 2'],
+                    'goles_local': int(goles[0]) + int(golesParte2[0]),
+                    'goles_visitante': int(goles[1]) + int(golesParte2[1])
+                })
     except:
-        print("ERROR: Ha ocurrido un error al acceder/analizar el archivo CSV.")
+        print("ERROR")
     
     return partidos
-
