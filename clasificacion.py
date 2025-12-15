f2857eee35b557ddb77e981c6868fcaa7f9aae03
Gabriela Alonso <185179280+E-Gabriela-Alonso-Amaya@users.noreply.github.com>
2025-12-15 13:23:57 +0100

    Función  que recibe una lista de partidos y devuelve los puntos obtenidos

--- clasificacion.py
+++ clasificacion.py
@@ -8,3 +8,10 @@ def equipos(datosliga) -> list[str]:
                 continue
             equipos.append(i)
     return equipos
+
+# Puntos(info): Función que recibe una lista con los partidos ganados, empatados y
+# perdidos y devuelve los puntos obtenidos. (Gabriela)
+def puntos(lista_partidos):
+    ganados = lista_partidos[0]
+    empatados = lista_partidos[1]
+    return 3 * ganados + empatados
